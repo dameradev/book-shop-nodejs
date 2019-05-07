b0bdf72afd010d710921448ae484425c70752170
=DamjanRadev <radevdamjan@gmail.com>
2019-05-07 17:45:44 +0200

    Enhancing the App

--- models/product.js
+++ models/product.js
@@ -2,38 +2,39 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-  path.dirname(process.mainModule.filename), 
-  'data', 
+  path.dirname(process.mainModule.filename),
+  'data',
   'products.json'
 );
 
 const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-     cb([]);
+      cb([]);
     } else {
       cb(JSON.parse(fileContent));
-    }  
+    }
   });
-  
-}
+};
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
     getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
-    })
-    
+    });
   }
 
-  static async fetchAll(cb) {
-     getProductsFromFile(cb);
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
-}
+};
