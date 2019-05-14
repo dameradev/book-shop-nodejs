5552751244ea65aefb4055720aec10733c88971e
=DamjanRadev <radevdamjan@gmail.com>
2019-05-14 13:31:38 +0200

    Finished edit functionality

--- routes/admin.js
+++ routes/admin.js
@@ -19,6 +19,6 @@ router.post('/add-product', adminController.postAddProduct);
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
 // /admin/edit-product => GET
-router.post('/edit-product/:productId', adminController.getEditProduct);
+router.post('/edit-product', adminController.postEditProduct);
 
 module.exports = router;
