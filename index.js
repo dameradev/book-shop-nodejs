31599d8d38b9159aae22bd3a88d597180c488eb1
=DamjanRadev <radevdamjan@gmail.com>
2019-05-15 22:02:42 +0200

    sequalize.sync(), Post route for creating a product

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequalize = require('./util/database');
 
 const app = express();
 
@@ -25,5 +25,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+sequalize.sync() // syncs models to tables into the database
+.then(result => {
+  // console.log(result);
+  app.listen(3000);
+})
+.then(err=>console.log(err));
 
-app.listen(3000);
+
+
