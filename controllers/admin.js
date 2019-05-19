ad1e27c1e848b51bcc184d33409a439a6bd7399e
=DamjanRadev <radevdamjan@gmail.com>
2019-05-19 20:38:19 +0200

    Select and populate example

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,11 +13,16 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product({ title, imageUrl, price, description });
+  const product = new Product({
+    title,
+    imageUrl,
+    price,
+    description,
+    userId: req.user
+  });
   product
     .save()
     .then(result => {
-      // console.log(result);
       console.log("Created Product");
       res.redirect("/admin/products");
     })
@@ -71,7 +76,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+    .select("title price -_id")
+    .populate("userId", "name")
     .then(products => {
+      console.log(products);
       res.render("admin/products", {
         prods: products,
         pageTitle: "Admin Products",
