7dc2c5ecbdd7b3039a7b63e111eaabd76c611222
=DamjanRadev <radevdamjan@gmail.com>
2019-05-19 16:21:39 +0200

    Delete functionali to remove products from cart

--- index.js
+++ index.js
@@ -1,55 +1,49 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+const errorController = require("./controllers/error");
+const mongoConnect = require("./util/database").mongoConnect;
 // const getDb = require('./util/database').getDb;
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 // Models
-const User = require('./models/user');
+const User = require("./models/user");
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("5ce14518e10db910805e7c0d")
-  .then(user => {
-    req.user = new User(user.username, user.email, user.cart, user._id);
-    next();
-  })
-  .catch(err=>console.log(err));
+  User.findById("5ce1649ea35e78f3da68d9d6")
+    .then(user => {
+      req.user = new User(user.username, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
-
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
-
-
 
 app.use(errorController.get404);
 
-
-mongoConnect(async (req) => {
+mongoConnect(async req => {
   // let user = await User.findById("5ce14518e10db910805e7c0d");
-//   if(req.user) {
-//     return re.user;
-//   } else{
-//     user = new User('dameradev', 'dame@radev.com');
-//     user.save();
-//     return user;
-// }
+  //   if(req.user) {
+  //     return re.user;
+  //   } else{
+  //     user = new User('dameradev', 'dame@radev.com');
+  //     user.save();
+  //     return user;
+  // }
   app.listen(3000);
 });
