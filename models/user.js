b187ca3c007eb60bfdc5f4503344087344e91cb5
=DamjanRadev <radevdamjan@gmail.com>
2019-05-19 16:48:50 +0200

    Get route for orders

--- models/user.js
+++ models/user.js
@@ -105,9 +105,10 @@ class User {
 
   getOrders() {
     const db = getDb();
-    // return db.collection('orders').then(orders=>{
-
-    // })
+    return db
+      .collection("orders")
+      .find({ "user._id": new mongodb.ObjectId(this._id) })
+      .toArray();
   }
 
   static findById(userId) {
