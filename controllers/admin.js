15d052dc7e0b46b3e303409d1afd02ab50466402
=DamjanRadev <radevdamjan@gmail.com>
2019-05-19 17:36:05 +0200

    Save data through mongoose, postAddProduct route

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,34 +1,25 @@
-const Product = require('../models/product');
-
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/edit-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
     editing: false
   });
 };
-
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(
-    title, 
-    price, 
-    description, 
-    imageUrl, 
-    null, 
-    req.user._id
-    );
+  const product = new Product({ title, imageUrl, price, description });
   product
     .save()
     .then(result => {
       // console.log(result);
-      console.log('Created Product');
-      res.redirect('/admin/products');
+      console.log("Created Product");
+      res.redirect("/admin/products");
     })
     .catch(err => {
       console.log(err);
@@ -37,23 +28,23 @@ exports.postAddProduct = (req, res, next) => {
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
-    res.redirect('/');
+    res.redirect("/");
   }
   const prodId = req.params.productId;
   Product.findById(prodId)
-  // Product.findByPk(prodId)
-  .then(product => {
-    if(!product) {
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
-  })
-  .catch(err => console.log(err));
+    // Product.findByPk(prodId)
+    .then(product => {
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -62,39 +53,42 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
   const updatedPrice = req.body.price;
-  
-    const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId)
-    
+
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    prodId
+  );
+
   product
-  .save()
-  .then(result => {
-    console.log('Updated Product!')
-    res.redirect('/admin/products');
-  })
-  .catch(err=>console.log(err));
-}
+    .save()
+    .then(result => {
+      console.log("Updated Product!");
+      res.redirect("/admin/products");
+    })
+    .catch(err => console.log(err));
+};
 
 exports.getProducts = (req, res, next) => {
-  
-  Product.fetchAll()
+  Product.find()
     .then(products => {
-      res.render('admin/products', {
+      res.render("admin/products", {
         prods: products,
-        pageTitle: 'Admin Products',
-        path: '/admin/products'
+        pageTitle: "Admin Products",
+        path: "/admin/products"
       });
     })
-    .catch(err=>console.log(err));
-  
-}
+    .catch(err => console.log(err));
+};
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.deleteById(prodId)
-  .then(() => {
-    console.log('Destroyed product');
-    res.redirect('/admin/products')
-  })
-  .catch(err=>console.log(err));
-  
-}
+    .then(() => {
+      console.log("Destroyed product");
+      res.redirect("/admin/products");
+    })
+    .catch(err => console.log(err));
+};
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 // Models
-const User = require("./models/user");
+// const User = require("./models/user");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -20,14 +20,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("5ce1649ea35e78f3da68d9d6")
-    .then(user => {
-      req.user = new User(user.username, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("5ce1649ea35e78f3da68d9d6")
+//     .then(user => {
+//       req.user = new User(user.username, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
