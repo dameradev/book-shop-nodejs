530d4e6db763a6bca05735cbb491a4423f977d94
=DamjanRadev <radevdamjan@gmail.com>
2019-05-19 21:01:29 +0200

    Displaying the cart and deleteing items from cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -39,8 +39,11 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate("cart.items.productId")
+    .execPopulate()
+    .then(user => {
+      console.log(user.cart.items);
+      const products = user.cart.items;
       res
         .render("shop/cart", {
           path: "/cart",
@@ -67,7 +70,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then(result => {
       res.redirect("/cart");
     })
