6d74e07b23d48c3cb2907066f3d8886fe505aa4a
=DamjanRadev <radevdamjan@gmail.com>
2019-05-21 17:00:11 +0200

    Added cookies for auth(startup code)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,18 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req
+    .get("Cookie")
+    .split(";")[2]
+    .trim()
+    .split("=")[1];
+
   res.render("auth/login", {
     path: "/login",
-    pageTitle: "Log in"
+    pageTitle: "Log in",
+    isAuthenticated: isLoggedIn
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.cookie("loggedIn", true);
+  res.redirect("/");
 };
--- index.js
+++ index.js
@@ -29,10 +29,9 @@ app.use((req, res, next) => {
     })
     .catch(err => console.log(err));
 });
-
+app.use(authRoutes);
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
-app.use(authRoutes);
 
 app.use(errorController.get404);
 
