e862e5f527230d68a41edeee6a7f57886ed1d243
=DamjanRadev <radevdamjan@gmail.com>
2019-05-27 18:56:36 +0200

    Adding validation for post login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,32 +46,38 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  User.findOne({ email })
-    .then(user => {
-      if (!user) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/login", {
+      path: "/login",
+      pageTitle: "Login",
+      errorMessage: errors.array()[0].msg
+    });
+  }
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      req.flash("error", "Invalid email or password.");
+      return res.redirect("/login");
+    }
+    bcrypt
+      .compare(password, user.password)
+      .then(doMatch => {
+        if (doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save(err => {
+            console.log(err);
+            res.redirect("/");
+          });
+        }
         req.flash("error", "Invalid email or password.");
-        return res.redirect("/login");
-      }
-      bcrypt
-        .compare(password, user.password)
-        .then(doMatch => {
-          if (doMatch) {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            return req.session.save(err => {
-              console.log(err);
-              res.redirect("/");
-            });
-          }
-          req.flash("error", "Invalid email or password.");
-          res.redirect("/login");
-        })
-        .catch(err => {
-          console.log(err);
-          res.redirect("/login");
-        });
-    })
-    .catch(err => console.log(err));
+        res.redirect("/login");
+      })
+      .catch(err => {
+        console.log(err);
+        res.redirect("/login");
+      });
+  });
 };
 
 exports.postSignup = (req, res, next) => {
