868dc40fa160d98fa88398a02f3a043dd2a26d15
=DamjanRadev <radevdamjan@gmail.com>
2019-05-27 19:18:28 +0200

    Keeping user input

--- controllers/auth.js
+++ controllers/auth.js
@@ -39,7 +39,8 @@ exports.getSignup = (req, res, next) => {
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
-    errorMessage: message
+    errorMessage: message,
+    oldInput: { email: "", password: "", confirmPassword: "" }
   });
 };
 
@@ -83,14 +84,15 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-
+  const confirmPassword = req.body.confirmPassword;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array());
     return res.status(422).render("auth/signup", {
       path: "/signup",
       pageTitle: "Signup",
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: { email, password, confirmPassword }
     });
   }
   bcrypt
