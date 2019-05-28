fcc9826be913d86d622ff6c11a41a73f10e61db6
=DamjanRadev <radevdamjan@gmail.com>
2019-05-28 20:00:40 +0200

    Async vs sync in middleware fucntion for throwing errors

--- index.js
+++ index.js
@@ -40,13 +40,20 @@ app.use(
 );
 app.use(csrfProtection);
 app.use(flash());
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use((req, res, next) => {
+  // We just throw error in sync code
   if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
     .then(user => {
+      // throw new Error("Dummy");
       if (!user) {
         return next();
       }
@@ -54,15 +61,10 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => {
-      throw new Error(err);
+      next(new Error(err)); // we use next in asycn code
     });
 });
 
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
-});
 app.use(authRoutes);
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -70,7 +72,11 @@ app.use(shopRoutes);
 app.get("/500", errorController.get500);
 app.use(errorController.get404);
 app.use((error, req, res, next) => {
-  res.redirect("/500");
+  res.status(500).render("500", {
+    pageTitle: "Error",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn
+  });
 });
 
 mongoose
